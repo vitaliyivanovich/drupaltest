eeaa58e89ff1ba5e1769492dfc6f30861e3d86fd
Vitaliy <vitaliydiscord@gmail.com>
2020-10-21 11:27:31 +0300

    Project Test Commit

---
+++ web/themes/custom/ww_bootstrap4/js/my.js
@@ -0,0 +1,41 @@
+jQuery( document ).ready(function() {
+  jQuery('.myslider').slick({
+    dots: false,
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
+
